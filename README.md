f35ff0a4136922de0fc45fb32877e0c33c10f8aa
Zachary Cecchetti <zcecchetti@gmail.com>
2022-05-09 22:57:06 -0700

    Created starting js, css, and html files, updated README, and set up webpack and eslint

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # WeatherApp
+
+The goal of this project is to create a web application that will 
+fetch data from a weather database's API and responsively display
+the data to the user.
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new ESLintPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
