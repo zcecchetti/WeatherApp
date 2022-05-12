f818a9148c28ee16bd304af32d55fd086809e658
Zachary Cecchetti <zcecchetti@gmail.com>
2022-05-11 20:50:07 -0700

    Page stores current day weather data in Current dictionary

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import './style.css';
 const apiKey = 'b28c7ed03e1d13347ecb843c2c580d4c';
 
 // create object that holds weather data for user input city
-// function weatherData(jsonCurrent, jsonForecast) {
-//   const { temp } = jsonCurrent.main;
-//   const { humidity } = jsonCurrent.main;
-//   const tempMax = jsonCurrent.main.temp_max;
-//   const tempMin = jsonCurrent.main.temp_min;
-//   return {
-//     temp, humidity, tempMax, tempMin, jsonForecast,
-//   };
-// }
+function weatherData(jsonForecast) {
+  const { temp } = jsonForecast.current;
+  const { humidity } = jsonForecast.current;
+  const today = jsonForecast.daily[0];
+  const tempMax = today.temp.max;
+  const tempMin = today.temp.min;
+  const current = {
+    temp, humidity, tempMax, tempMin,
+  };
+
+  return {
+    current,
+  };
+}
 
 // async function to get weather data from API and create object to store data
 async function getWeather(longitude, latitude) {
   try {
-    const weather = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`, { mode: 'cors' });
+    const weather = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts&appid=${apiKey}&units=imperial`, { mode: 'cors' });
     const jsonForecast = await weather.json();
-    // const userWeather = weatherData(jsonForecast);
-    console.log('current weather:', jsonForecast);
+    const userWeather = weatherData(jsonForecast);
+    console.log('current weather:', userWeather);
   } catch {
     console.log('err');
   }
