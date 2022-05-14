23988cba54f7724a5499f93066ab10c1dda9dc9a
Zachary Cecchetti <zcecchetti@gmail.com>
2022-05-13 17:08:56 -0700

    Added autocompleting search bar to allow user to select location and call getWeather function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 import './style.css';
 import { format, addDays } from 'date-fns';
+import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete';
 
 const apiKey = 'b28c7ed03e1d13347ecb843c2c580d4c';
 
@@ -22,14 +24,13 @@ function weatherData(jsonForecast) {
 
   for (let i = 1; i < 8; i += 1) {
     const day = jsonForecast.daily[i];
-    const futureTemp = day.temp.day;
     const futureMax = day.temp.max;
     const futureMin = day.temp.min;
     const skyWeather = day.weather[0];
     const futureSky = skyWeather.main;
     const futureDay = format(addDays(new Date(), i), 'EEEE, MMMM d');
     futureWeather[i - 1] = {
-      futureTemp, futureMax, futureMin, futureSky, futureDay,
+      futureMax, futureMin, futureSky, futureDay,
     };
   }
 
@@ -39,32 +40,26 @@ function weatherData(jsonForecast) {
 }
 
 // async function to get weather data from API and create object to store data
-async function getWeather(longitude, latitude) {
+async function getWeather(location) {
   try {
+    const longitude = location.properties.lon;
+    const latitude = location.properties.lat;
     const weather = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts&appid=${apiKey}&units=imperial`, { mode: 'cors' });
     const jsonForecast = await weather.json();
     const userWeather = weatherData(jsonForecast);
-    console.log('current weather:', jsonForecast, userWeather);
+    console.log('current weather:', userWeather);
   } catch {
     console.log('err');
   }
 }
 
-// async function to get longitude and latitude from API using user input city
-async function getLocationAndWeather(city, state, country) {
-  const userState = state.toLowerCase();
-  const userCountry = country.toLowerCase();
-  try {
-    const location = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${userState},${userCountry}&limit=1&appid=${apiKey}`, { mode: 'cors' });
-    const geoJson = await location.json();
-    const cityLoc = geoJson[0];
-    const longitude = cityLoc.lon;
-    const latitude = cityLoc.lat;
-    getWeather(longitude, latitude);
-  } catch {
-    console.log('err');
-  }
-}
-
-// call functions
-getLocationAndWeather('stockton', 'ca', 'us');
+// generate autocompleting search bar to find longitude and latitude then call getWeather function
+const autocomplete = new GeocoderAutocomplete(
+  document.getElementById('autocomplete'),
+  'b0a58b077de948e0a4085085cffda14b',
+  { type: 'city' },
+);
+autocomplete.on('select', (location) => {
+  getWeather(location);
+//   getLocationAndWeather(location);
+});
